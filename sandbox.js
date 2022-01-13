4ec1e3a82cc24db0117a0fba744d03cc9ea11ddc
JiaXuan Hon <46828701+idanceinbetween@users.noreply.github.com>
2022-01-13 21:05:14 +0100

    Refactor : Put it in a 'box' (array)

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,22 @@
+// function nextCharForNumberString(str) {
+//     const trimmed = str.trim()
+//     const number = parseInt(trimmed)
+//     const nextNumber = number + 1
+//     return String.fromCharCode(nextNumber)
+// }
+
+// Put the parameter in a box
 function nextCharForNumberString(str) {
-    const trimmed = str.trim()
-    const number = parseInt(trimmed)
-    const nextNumber = number + 1
-    return String.fromCharCode(nextNumber)
+    // map enables composability
+    // Each expression has its own state completely contained
+    return [str]
+        .map(s => s.trim())
+        .map(s => parseInt(s))
+        .map(i => i + 1)
+        .map(i => String.fromCharCode(i))
 }
 
 const result = nextCharForNumberString(' 64')
 console.log(result)
-// A
+// [ 'A' ]
+// The output is also in a box
